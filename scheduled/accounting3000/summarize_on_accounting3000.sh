0363ab1c94e9f8089247edbedd9b3aa83d9bae7c
Cannon Lock <clock@wisc.edu>
2025-01-28 14:10:23 -0600

    Add summary script and scheduled scripts

--- scheduled/accounting3000/summarize_on_accounting3000.sh
+++ scheduled/accounting3000/summarize_on_accounting3000.sh
@@ -4,4 +4,4 @@ cd /home/clock/ospool-summary
 
 source .venv/bin/activate
 
-python3 -m cli summarize --env-file .env $(date +%Y-%m-%d)
+python3 -m cli summarize --env-file .env $(date -d "yesterday" +%Y-%m-%d)
